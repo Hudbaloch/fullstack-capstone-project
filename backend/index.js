6a6fceea7d59082506de85440f6c1ae8e3cd10d0
Huda Batool <ig.hudabatool@gmail.com>
2026-08-27 17:33:06 +0500

    Add server entry point

---
+++ backend/index.js
@@ -0,0 +1,8 @@
+const app = require("./app");
+const natural = require("natural");
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`GiftLink server running on port ${PORT}`);
+});
