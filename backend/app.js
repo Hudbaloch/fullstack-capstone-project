b6f23e919ef80b741aa2e4cb101d24efc5cf8fc4
Huda Batool <ig.hudabatool@gmail.com>
2026-08-27 17:32:20 +0500

    Add Express app and search route

---
+++ backend/app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const cors = require("cors");
+
+const giftRoutes = require("./giftRoutes");
+const searchRoutes = require("./searchRoutes");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use(giftRoutes);
+app.use(searchRoutes);
+
+app.get("/", (req, res) => {
+  res.send("GiftLink API is running");
+});
+
+module.exports = app;
